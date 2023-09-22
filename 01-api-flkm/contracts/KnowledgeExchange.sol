9e76294fee6600a5a5a16e4e3229d05187069bd2
Steven Robbins <steven.robbins.000@gmail.com>
2023-09-22 16:03:08 -0500

    Update KnowledgeExchange.sol

--- 01-api-flkm/contracts/KnowledgeExchange.sol
+++ 01-api-flkm/contracts/KnowledgeExchange.sol
@@ -1,410 +1,105 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "../../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-// Define the Knowledge Exchange Contract
-contract KnowledgeExchangeContract is Ownable {
+contract KnowledgeExchangeContract is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
-    // State variables
     string public contractName;
-    IERC20 public knowledgeToken; // Native token of FLKM
+    IERC20 public knowledgeToken;
 
-    // State variable to store user reputation
-    mapping(address => string) public userReputation;
-    // Mapping to associate each level with its title
-    mapping(ReputationLevel => string) public levelTitles;
-    // Mapping for reputation bonus values
+    enum ReputationLevel { Mystic, Luminary, Oracle, Prophet }
+
+    mapping(address => ReputationLevel) public userReputation;
     mapping(ReputationLevel => uint256) public reputationBonusValues;
+    mapping(address => uint256) public userResourceCount;
+    mapping(address => uint256) public userTotalDemand;
 
-    // 02 - Reputation and Bonus ***********************************************
-    enum ReputationLevel {
-        Mystic, // 0
-        Luminary, // 1
-        Oracle, // 2
-        Prophet // 3
-    }
-
-    // 03 - Data *****************************************************************
-    // Structure to represent a knowledge resource
-    // Data
     struct KnowledgeResource {
         address creator;
         string title;
         string description;
-        uint256 demand; // Score for the resource
+        uint256 demand;
         bool exists;
     }
 
-    // Mapping to store knowledge resources
     mapping(uint256 => KnowledgeResource) public knowledgeResources;
     uint256 public nextResourceID = 1;
 
-    // Models
-    struct Model {
-        address owner;
-        uint256 creationDate;
-        uint256 shelfLife;
-        uint256 price;
-        string description; // Description of what the model offers.
-        uint256 qualityScore; // Could be an average of community reviews or other metrics.
-        bool isEthical; // A flag to show if the model meets ethical standards.
-        uint256 endorsements; // Number of users that have endorsed the model.
-    }
-
-    struct Review {
-        address reviewer;
-        uint256 modelId;
-        uint256 score; // Rating score for the model.
-        string comment; // Feedback about the model.
-    }
-
-    mapping(uint256 => Model) public models;
-    mapping(uint256 => Review[]) public modelReviews;
-
-    uint256 public nextModelId = 1;
-
-    // 04 - Compensation for Data **********************************************************
     uint256 constant MINIMUM_COMPENSATION = 10;
+    uint256 constant MAX_DEMAND = 1000;
+    uint256 constant UNIQUENESS_SCALING_FACTOR = 10;
+    uint256 constant CONTRIBUTION_SCALING_FACTOR = 5;
 
-    // Events to log important contract actions
-    event ResourceUploaded(
-        uint256 indexed resourceID,
-        address indexed creator,
-        string title,
-        string description
-    );
+    event ResourceUploaded(uint256 indexed resourceID, address indexed creator, string title, string description);
     event ResourceAccessed(uint256 indexed resourceID, address indexed user);
 
-    // 10 - Helpers *************************************************************
-    uint256 constant MAX_DEMAND = 1000; // An arbitrary cap on how much demand a resource can have.
-    uint256 constant UNIQUENESS_SCALING_FACTOR = 10; // A scaling factor to make uniqueness calculations meaningful.
-    uint256 constant CONTRIBUTION_SCALING_FACTOR = 5; // A scaling factor for calculating user contributions.
-
-    // Event to log when the contract receives Ether
-    event FundsReceived(address indexed sender, uint256 amount);
-
-    // Constructor to initialize the contract
     constructor(string memory _contractName, address _knowledgeTokenAddress) {
         contractName = _contractName;
         knowledgeToken = IERC20(_knowledgeTokenAddress);
-        levelTitles[ReputationLevel.Mystic] = "Mystic";
-        levelTitles[ReputationLevel.Luminary] = "Luminary";
-        levelTitles[ReputationLevel.Oracle] = "Oracle";
-        levelTitles[ReputationLevel.Prophet] = "Prophet";
     }
 
-    // 02 - Reputation ************************************************************************************************
-    // Function to get the reputation rank of a user
-    function getUserReputation(
-        address _user
-    ) internal view returns (string memory) {
-        // Implement the logic to fetch and determine the reputation rank of a user.
-        // Reputation ranks may be based on user activities, reviews, and engagement.
-        // Return the reputation rank as a string (e.g., "Mystic," "Luminary," "Oracle," "Prophet").
-        // Users with higher reputation ranks receive reputation-based bonuses.
-        // Users with lower reputation ranks receive lower bonuses or none.
-        // Adjust the logic based on platform goals and user interactions.
-        //return "Mystic"; // Placeholder value, replace with actual
-        return userReputation[_user];
-    }
-
-    // Function to get reputation level from string
-    function getReputationLevel(
-        string memory _userReputation
-    ) internal view returns (ReputationLevel) {
-        for (uint8 i = 0; i < 4; i++) {
-            if (
-                keccak256(abi.encodePacked(_userReputation)) ==
-                keccak256(abi.encodePacked(levelTitles[ReputationLevel(i)]))
-            ) {
-                return ReputationLevel(i);
-            }
-        }
-        revert("Invalid reputation string provided");
-    }
-
-    // Function to update a user's reputation based on their activity or achievements
-    function updateUserReputation(
-        address _user,
-        ReputationLevel _newLevel
-    ) external onlyOwner {
-        // Check if the new level is higher than the current level
-        require(
-            (bytes(userReputation[_user]).length == 0 ||
-                keccak256(bytes(userReputation[_user])) <
-                keccak256(bytes(levelTitles[_newLevel]))),
-            "Cannot decrease reputation level"
-        );
-
-        // Update the user's reputation
-        userReputation[_user] = levelTitles[_newLevel];
-    }
-
-    // Function to calculate reputation-based bonus
-    function calculateReputationBonus(
-        string memory _userReputation
-    ) internal view returns (uint256) {
-        ReputationLevel level = getReputationLevel(_userReputation);
-
-        // Return the bonus associated with the reputation level.
-        return reputationBonusValues[level];
-    }
-
-    // 03 - Data ************************************************************************************************
-    // Function to upload a knowledge resource
-    function uploadResource(
-        string memory _title,
-        string memory _description
-    ) external {
-        require(
-            knowledgeToken.balanceOf(msg.sender) >= 100,
-            "Insufficient knowledge tokens"
-        );
+    function uploadResource(string memory _title, string memory _description) external {
+        require(knowledgeToken.balanceOf(msg.sender) >= 100, "Insufficient knowledge tokens");
 
         knowledgeResources[nextResourceID] = KnowledgeResource({
             creator: msg.sender,
             title: _title,
             description: _description,
             demand: 0,
-            exists: true // Set the resource as existing
+            exists: true
         });
 
+        userResourceCount[msg.sender]++;
         emit ResourceUploaded(nextResourceID, msg.sender, _title, _description);
         nextResourceID++;
     }
 
-    function accessResource(uint256 _resourceID) external {
-        require(
-            knowledgeResources[_resourceID].exists,
-            "Resource does not exist"
-        );
+    function accessResource(uint256 _resourceID) external nonReentrant {
+        require(knowledgeResources[_resourceID].exists, "Resource does not exist");
 
         knowledgeResources[_resourceID].demand++;
+        userTotalDemand[knowledgeResources[_resourceID].creator]++;
 
         uint256 compensationAmount = calculateCompensation(_resourceID);
-        knowledgeToken.transferFrom(
-            msg.sender,
-            knowledgeResources[_resourceID].creator,
-            compensationAmount
-        );
+        knowledgeToken.transferFrom(msg.sender, knowledgeResources[_resourceID].creator, compensationAmount);
 
         emit ResourceAccessed(_resourceID, msg.sender);
     }
 
-    // 04 - Compensation for Data *********************************************************************************
-    // Calculate compensation for accessing a resource
-    function calculateCompensation(
-        uint256 _resourceID
-    ) internal view returns (uint256) {
-        // Error handling for potential non-existing resource
-        require(
-            knowledgeResources[_resourceID].exists,
-            "Resource does not exist"
-        );
+    function calculateCompensation(uint256 _resourceID) internal view returns (uint256) {
+        require(knowledgeResources[_resourceID].exists, "Resource does not exist");
 
-        // Resource Demand: Resources with higher demand receive higher compensation.
-        // Demand can be determined by the number of accesses and resource importance.
         uint256 demand = knowledgeResources[_resourceID].demand;
+        uint256 reputationBonus = reputationBonusValues[userReputation[msg.sender]];
+        uint256 uniquenessFactor = MAX_DEMAND.sub(demand).mul(UNIQUENESS_SCALING_FACTOR);
+        uint256 contributionBonus = userResourceCount[msg.sender].mul(CONTRIBUTION_SCALING_FACTOR).add(userTotalDemand[msg.sender]);
 
-        // User Reputation: Users with higher reputation ranks receive a reputation-based bonus.
-        // Reputation ranks encourage users to contribute positively to the platform.
-        uint256 reputationBonus = calculateReputationBonus(
-            getUserReputation(msg.sender)
-        );
-
-        // Resource Uniqueness: Unique or rare resources receive higher compensation.
-        uint256 uniquenessFactor = calculateUniquenessFactor(_resourceID);
-
-        // Contributions: Users who contribute significantly to the platform may receive extra compensation.
-        uint256 contributionBonus = calculateContributionBonus(msg.sender);
-
-        // Simplify compensation calculation and ensure minimum compensation
-        uint256 compensation = demand +
-            reputationBonus +
-            uniquenessFactor +
-            contributionBonus;
-
-        return
-            (compensation < MINIMUM_COMPENSATION)
-                ? MINIMUM_COMPENSATION
-                : compensation;
+        uint256 compensation = demand.add(reputationBonus).add(uniquenessFactor).add(contributionBonus);
+        return (compensation < MINIMUM_COMPENSATION) ? MINIMUM_COMPENSATION : compensation;
     }
 
-    // Function to calculate resource uniqueness factor
-    function calculateUniquenessFactor(
-        uint256 _resourceID
-    ) internal view returns (uint256) {
-        // Ensure the resource exists before calculating its uniqueness factor
-        require(
-            knowledgeResources[_resourceID].exists,
-            "Resource does not exist"
-        );
-
-        // Here, I'm using the `demand` as a proxy for rarity:
-        // rarer resources might have less demand (but this is a simplification).
-        uint256 rarityFactor = MAX_DEMAND.sub(
-            knowledgeResources[_resourceID].demand
-        );
-
-        // For true uniqueness you'd need other metrics, perhaps an "originality" score,
-        // number of similar resources, etc. but those would need new data structures or external systems.
-
-        // Just as an example, a scaling factor is applied to ensure this value remains meaningful.
-        return rarityFactor.mul(UNIQUENESS_SCALING_FACTOR); // Placeholder, replace with actual logic.
+    function updateUserReputation(address _user, ReputationLevel _newLevel) external onlyOwner {
+        require(userReputation[_user] < _newLevel, "Cannot decrease reputation level");
+        userReputation[_user] = _newLevel;
     }
 
-    // Function to count the number of resources uploaded by a given user.
-    function countUserResources(
-        address _userAddress
-    ) internal view returns (uint256) {
-        uint256 count = 0;
-        // Iterate through all resources. This can be expensive in terms of gas!
-        for (uint256 i = 1; i < nextResourceID; i++) {
-            if (knowledgeResources[i].creator == _userAddress) {
-                count++;
-            }
-        }
-        return count;
-
-        // NOTE: This method isn't optimal due to the potential high gas cost.
-        // Ideally, you'd maintain a separate mapping to keep track of resources per user,
-        // or a counter incremented every time a user uploads a new resource.
+    function updateReputationBonusValue(ReputationLevel _level, uint256 _value) external onlyOwner {
+        reputationBonusValues[_level] = _value;
     }
 
-    // Function to calculate the total demand of all resources uploaded by a given user.
-    function calculateTotalUserDemand(
-        address _userAddress
-    ) internal view returns (uint256) {
-        uint256 totalDemand = 0;
-        // Iterate through all resources. This can be expensive in terms of gas!
-        for (uint256 i = 1; i < nextResourceID; i++) {
-            if (knowledgeResources[i].creator == _userAddress) {
-                totalDemand += knowledgeResources[i].demand;
-            }
-        }
-        return totalDemand;
-
-        // NOTE: Similar to the above function, iterating through all resources is costly.
-        // You might consider maintaining a separate mapping or state variable to store the cumulative
-        // demand for each user. This way, you can update it incrementally rather than recalculating.
-    }
-
-    // Function to calculate contribution-based bonus for a user
-    function calculateContributionBonus(
-        address _userAddress
-    ) internal view returns (uint256) {
-        // This might be a function of the number of resources uploaded, their demand, and perhaps
-        // other metrics related to the user's activity on the platform.
-
-        uint256 resourcesUploaded = countUserResources(_userAddress); // You'd need to implement countUserResources
-        uint256 totalUserDemand = calculateTotalUserDemand(_userAddress); // Another function you might need
-
-        // Here, the bonus is proportional to the resources uploaded and their overall demand.
-        return
-            resourcesUploaded.mul(CONTRIBUTION_SCALING_FACTOR).add(
-                totalUserDemand
-            );
-
-        // Alternatively, you could have a more intricate function taking into account other
-        // contributions such as reviews, ratings, collaborations, etc.
-    }
-
-    // 11 - Models *********************************************************************************************************
-    // Add compensation, payment, or incorporated into data compensation logic
-    // Allow users to add their models to the marketplace.
-    function addModel(
-        uint256 _shelfLife,
-        uint256 _price,
-        string memory _description
-    ) external {
-        Model memory newModel = Model({
-            owner: msg.sender,
-            creationDate: block.timestamp,
-            shelfLife: _shelfLife,
-            price: _price,
-            description: _description,
-            qualityScore: 0,
-            isEthical: false,
-            endorsements: 0
-        });
-
-        models[nextModelId] = newModel;
-        nextModelId++;
-    }
-
-    // Allow users to review and rate models.
-    function reviewModel(
-        uint256 _modelId,
-        uint256 _score,
-        string memory _comment
-    ) external {
-        Review memory newReview = Review({
-            reviewer: msg.sender,
-            modelId: _modelId,
-            score: _score,
-            comment: _comment
-        });
-
-        modelReviews[_modelId].push(newReview);
-
-        // Update the model's quality score. For simplicity, just taking an average here.
-        models[_modelId].qualityScore =
-            (models[_modelId].qualityScore + _score) /
-            2;
-    }
-
-    // Function for purchasing the model. For simplicity, this will transfer funds.
-    function purchaseModel(uint256 _modelId) external payable {
-        require(
-            msg.value == models[_modelId].price,
-            "Incorrect payment amount."
-        );
-        require(
-            block.timestamp <=
-                models[_modelId].creationDate + models[_modelId].shelfLife,
-            "Model has expired."
-        );
-
-        // Transfer the funds to the model's owner.
-        payable(models[_modelId].owner).transfer(msg.value);
-
-        // A more complex implementation might provide the buyer with some token or access right.
-    }
-
-    // Endorse a model.
-    function endorseModel(uint256 _modelId) external {
-        models[_modelId].endorsements++;
-
-        // You could add logic here to reward endorsers or increase the model's visibility/rank.
-    }
-
-    // Check if a model meets ethical guidelines. Create guidelines that are voted in and used to rate?
-    // For the sake of this example, we're making it a manual check by the contract owner, but it could be more decentralized.
-    function markModelAsEthical(uint256 _modelId) external onlyOwner {
-        models[_modelId].isEthical = true;
-    }
-
-    // 12 - Compensation for Models ****************************************************************************************************
-    // ...
-
-
-
-    
-    // Update the contract name (only callable by the contract owner)
     function updateContractName(string memory _newName) external onlyOwner {
         contractName = _newName;
     }
 
-    // Fallback function to receive Ether
-    fallback() external payable {
+    // Function to accept Ether. Emit an event to log the received Ether.
+    receive() external payable {
         emit FundsReceived(msg.sender, msg.value);
     }
 
-    receive() external payable {
-        // custom function code
-    }
+    event FundsReceived(address indexed sender, uint256 amount);
 }
