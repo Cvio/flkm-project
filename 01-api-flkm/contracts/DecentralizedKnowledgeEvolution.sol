0972687c295277937651fd8e974c799ba5f1ddd9
Steven Robbins <steven.robbins.000@gmail.com>
2023-09-22 15:09:07 -0500

    Update DecentralizedKnowledgeEvolution.sol

--- 01-api-flkm/contracts/DecentralizedKnowledgeEvolution.sol
+++ 01-api-flkm/contracts/DecentralizedKnowledgeEvolution.sol
@@ -1,1 +1,91 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DecentralizedKnowledgeEvolution is Ownable {
+    
+    struct Knowledge {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 upvotes;
+    }
+    
+    struct ModificationProposal {
+        uint256 knowledgeId;
+        address proposer;
+        string newContent;
+        uint256 votes;
+    }
+    
+    Knowledge[] public knowledgeBase;
+    ModificationProposal[] public modificationProposals;
+    
+    mapping(address => uint256) public rewards;
+    mapping(uint256 => mapping(address => bool)) public knowledgeVoters;
+    mapping(uint256 => mapping(address => bool)) public proposalVoters;
+    
+    IERC20 public rewardToken;
+    
+    constructor(address _rewardToken) {
+        rewardToken = IERC20(_rewardToken);
+    }
+
+    function contributeKnowledge(string memory _content) external {
+        Knowledge memory newKnowledge;
+        newKnowledge.author = msg.sender;
+        newKnowledge.content = _content;
+        newKnowledge.timestamp = block.timestamp;
+        knowledgeBase.push(newKnowledge);
+        
+        // Reward logic here
+        // Example: rewards[msg.sender] += 10;
+    }
+
+    function upvoteKnowledge(uint256 _knowledgeId) external {
+        require(!knowledgeVoters[_knowledgeId][msg.sender], "Already voted");
+        knowledgeBase[_knowledgeId].upvotes += 1;
+        knowledgeVoters[_knowledgeId][msg.sender] = true;
+        
+        // Reward logic here
+        // Example: rewards[msg.sender] += 1; rewards[knowledgeBase[_knowledgeId].author] += 1;
+    }
+    
+    function proposeModification(uint256 _knowledgeId, string memory _newContent) external {
+        ModificationProposal memory newProposal;
+        newProposal.knowledgeId = _knowledgeId;
+        newProposal.proposer = msg.sender;
+        newProposal.newContent = _newContent;
+        modificationProposals.push(newProposal);
+    }
+
+    function voteOnModification(uint256 _modificationId) external {
+        require(!proposalVoters[_modificationId][msg.sender], "Already voted");
+        modificationProposals[_modificationId].votes += 1;
+        proposalVoters[_modificationId][msg.sender] = true;
+        
+        // Potential implementation to automatically approve modification if votes reach a certain threshold
+        // if(modificationProposals[_modificationId].votes >= threshold) implementModification(_modificationId);
+    }
+
+    function implementModification(uint256 _modificationId) external {
+        // Only allow to implement if votes are above a certain threshold
+        // Require statements to validate the implementation conditions
+        Knowledge storage original = knowledgeBase[modificationProposals[_modificationId].knowledgeId];
+        original.content = modificationProposals[_modificationId].newContent;
+    }
+
+    function claimRewards() external {
+        uint256 reward = rewards[msg.sender];
+        require(reward > 0, "No rewards available");
+        rewards[msg.sender] = 0;
+        rewardToken.transfer(msg.sender, reward);
+    }
+
+    function updateParameter() external onlyOwner {
+        // Implementation details to update parameters, such as changing the reward amounts, threshold for modification approvals, etc.
+    }
+}
