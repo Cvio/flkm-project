f76ef8d903346428a68a6f7372c3a579919df2f5
Steven Robbins <steven.robbins.000@gmail.com>
2023-09-26 21:13:21 -0500

    marketplace create - resource list starting to work. also roadmap with presentation items

--- 01-api-flkm/routes/resource-routes.js
+++ 01-api-flkm/routes/resource-routes.js
@@ -33,6 +33,7 @@ resourceRoutes.post(
   upload.single("file"),
   async (req, res) => {
     try {
+      const ownerId = req.body.ownerId; // Get the owner ID from the request body
       const resourceId = uuidv4(); // Generate a unique Resource ID
       const filePath = req.file.path; // Get the path of the uploaded file
       const buffer = req.file.buffer; // Get the buffer of the uploaded file
@@ -40,7 +41,9 @@ resourceRoutes.post(
       const results = [];
       const Schema = mongoose.Schema;
       const DynamicSchema = new Schema(
-        {},
+        {
+          ownerId: { type: String, required: true },
+        },
         { strict: false, collection: resourceId }
       );
       const DynamicModel = mongoose.model(resourceId, DynamicSchema);
@@ -48,7 +51,10 @@ resourceRoutes.post(
       // Parse CSV from file path
       fs.createReadStream(filePath)
         .pipe(csvParser())
-        .on("data", (data) => results.push(data))
+        .on("data", (data) => {
+          data.ownerId = ownerId;
+          results.push(data);
+        })
         .on("end", async () => {
           // Store in MongoDB
           await DynamicModel.insertMany(results);
@@ -76,5 +82,44 @@ resourceRoutes.post(
     }
   }
 );
+
+resourceRoutes.get("/resource-list", async (req, res) => {
+  try {
+    // Get the list of all collections (dynamic tables) in the database
+    const collectionNames = await mongoose.connection.db
+      .listCollections()
+      .toArray();
+
+    let resources = [];
+
+    for (const collection of collectionNames) {
+      // Create a dynamic model for each collection
+      const DynamicModel = mongoose.model(
+        collection.name,
+        new mongoose.Schema({}, { strict: false })
+      );
+
+      // Retrieve documents (or any other necessary data) from the dynamic collection
+      const docs = await DynamicModel.find({}).lean(); // .lean() for performance, optional
+      resources.push({ collectionName: collection.name, data: docs }); // Adjust the structure as needed
+    }
+
+    res.status(200).json(resources);
+  } catch (error) {
+    console.error("Error fetching resources: ", error);
+    res.status(500).send("Error fetching resources");
+  }
+});
+
+resourceRoutes.get("/fetch-resources/:resourceId", async (req, res) => {
+  try {
+    const { resourceId } = req.params;
+    const Model = mongoose.model(resourceId); // Get the model by resourceId
+    const resources = await Model.find({}); // Fetch all documents from the collection
+    res.status(200).json(resources); // Send the fetched resources as JSON
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 module.exports = resourceRoutes;
