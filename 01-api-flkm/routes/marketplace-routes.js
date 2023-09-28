dbcc57ac7e684d1f424fc584d8aea9663f7fa669
Steven Robbins <steven.robbins.000@gmail.com>
2023-09-27 21:04:25 -0500

    fix api

--- 01-api-flkm/routes/marketplace-routes.js
+++
@@ -1,7 +0,0 @@
-// Assuming you have an Express Router
-const marketPlaceRoutes = require("express").Router();
-const MarketplaceController = require("../controllers/marketplace.controller");
-
-marketPlaceRoutes.get("/resource-list", MarketplaceController.getResourceList);
-
-module.exports = marketPlaceRoutes;
