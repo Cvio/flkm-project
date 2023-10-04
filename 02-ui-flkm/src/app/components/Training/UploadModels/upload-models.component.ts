5c4cc0324c76d02fa722c5e46c924b472c44a9a2
Steven Robbins <steven.robbins.000@gmail.com>
2023-10-03 20:08:32 -0500

    compdoc test

---
+++ 02-ui-flkm/src/app/components/Training/UploadModels/upload-models.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'upload-models-component',
+  templateUrl: './upload-models.component.html',
+  styleUrls: ['./upload-models.component.css'],
+})
+export class UploadModelsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
