736e8a1c3f92291081c29ebc353f8ede6d449d3a
Steven Robbins <steven.robbins.000@gmail.com>
2023-10-26 18:02:53 -0500

    marketplace base contract test

--- contracts/Core/CowlMarketplace.sol
+++ contracts/Core/CowlMarketplace.sol
@@ -1,17 +1,12 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.17;
-// import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-import "../../node_modules/@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-import "../../node_modules/@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "../../node_modules/@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
-import "../../node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
-contract CowlMarketplace is
+contract CowlMarketplaceBase is
     Initializable,
     AccessControlUpgradeable,
     ReentrancyGuardUpgradeable
--- migrations/2_deploy_core.js
+++ migrations/2_deploy_core.js
@@ -1,5 +1,14 @@
-const CowlToken = artifacts.require("CowlToken"); // Replace "CoreContract" with the actual name of your core contract
+const CowlToken = artifacts.require("CowlToken");
+const CowlMarketplaceBase = artifacts.require("CowlMarketplaceBase");
 
-module.exports = function (deployer) {
-  deployer.deploy(CowlToken);
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(CowlToken);
+  const cowlToken = await CowlToken.deployed();
+
+  // deploy the marketplace
+  await deployer.deploy(CowlMarketplaceBase);
+  const cowlMarketplaceBase = await CowlMarketplaceBase.deployed();
+
+  // initialize the marketplace with the address of the token
+  await cowlMarketplaceBase.initialize(cowlToken.address);
 };
