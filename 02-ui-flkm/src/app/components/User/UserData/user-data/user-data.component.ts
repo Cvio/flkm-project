34257601b2810ff6990dcdb04352e052f489c7ff
Steven Robbins <steven.robbins.000@gmail.com>
2023-10-31 15:03:30 -0500

    backtrack to workout ipfs issues

--- 02-ui-flkm/src/app/components/User/UserData/user-data/user-data.component.ts
+++ 02-ui-flkm/src/app/components/User/UserData/user-data/user-data.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../../services/User/UserData/user-data.service';
-import IPFS from 'ipfs';
+// import { create } from 'ipfs';
 
 @Component({
   selector: 'app-user-data',
@@ -14,7 +14,7 @@ export class UserDataComponent implements OnInit {
   constructor(private userService: UserService) {}
 
   async ngOnInit(): Promise<void> {
-    this.ipfsNode = await IPFS.create();
+    //this.ipfsNode = await create();
   }
 
   onFileSelected(event: Event): void {
@@ -25,32 +25,31 @@ export class UserDataComponent implements OnInit {
     }
   }
 
-  async uploadToIPFS(file: File): Promise<string> {
-    try {
-      const fileBuffer = await file.arrayBuffer();
-      const result = await this.ipfsNode.add(fileBuffer);
-      return result.cid.toString();
-    } catch (error) {
-      console.error('Error uploading to IPFS:', error);
-      return '';
-    }
-  }
+  // async uploadToIPFS(file: File): Promise<string> {
+  //   try {
+  //     const fileBuffer = await file.arrayBuffer();
+  //     const result = await this.ipfsNode.add(fileBuffer);
+  //     return result.cid.toString();
+  //   } catch (error) {
+  //     console.error('Error uploading to IPFS:', error);
+  //     return '';
+  //   }
+  // }
 
   async onSubmit(): Promise<void> {
     if (this.selectedFile) {
       // Upload the file to IPFS and mint the NFT
-      const cid = await this.uploadToIPFS(this.selectedFile);
-
-      if (cid) {
-        // Mint the NFT
-        this.userService.mintNFT(cid).then((result) => {
-          if (result) {
-            console.log('NFT minted successfully');
-          } else {
-            console.log('Failed to mint NFT');
-          }
-        });
-      }
+      // const cid = await this.uploadToIPFS(this.selectedFile);
+      // if (cid) {
+      //   // Mint the NFT
+      //   this.userService.mintNFT(cid).then((result) => {
+      //     if (result) {
+      //       console.log('NFT minted successfully');
+      //     } else {
+      //       console.log('Failed to mint NFT');
+      //     }
+      //   });
+      // }
     }
   }
 }
